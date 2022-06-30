410710909ded40eda43eee8ed306feedc222b2f3
Manning <57797506+limanningforpages@users.noreply.github.com>
2022-06-30 11:48:06 +0800

    Update prefer_color_scheme_js.js

--- js/prefer_color_scheme_js.js
+++ js/prefer_color_scheme_js.js
@@ -1,13 +1,17 @@
-let dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-console.log(matched);
-
 let body = document.querySelector("body");
 let content = document.getElementById("content");
 
-if (dark) {
-	body.style.backgroundColor = "#green";
-	content.setAttribute("class", "dark");
-} else {
-	body.style.backgroundColor = "#blue";
-	content.setAttribute("class", "light");
-}
+window
+	.matchMedia("(prefers-color-scheme: dark)")
+	.addEventListener("change", function (e) {
+		const colorScheme = e.matches ? "dark" : "light";
+		console.log(colorScheme);
+
+		if (colorScheme === "dark") {
+			body.style.backgroundColor = "#green";
+			content.setAttribute("class", "dark");
+		} else {
+			body.style.backgroundColor = "#blue";
+			content.setAttribute("class", "light");
+		}
+	});
